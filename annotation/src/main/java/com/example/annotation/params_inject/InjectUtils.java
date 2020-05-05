be173ddc90049d7dee7bbb901f131fc912f8e09c
justinworkshop <justinworkshop@163.com>
2020-05-06 00:30:01 +0800

    更新InjectUtils

--- annotation/src/main/java/com/example/annotation/params_inject/InjectUtils.java
+++ annotation/src/main/java/com/example/annotation/params_inject/InjectUtils.java
@@ -25,19 +25,19 @@ public class InjectUtils {
     public static void injectView(Activity activity) {
         Class<? extends Activity> cls = activity.getClass();
 
-        //获得此类所有的成员
+        // 获得此类所有的成员
         Field[] declaredFields = cls.getDeclaredFields();
         for (Field filed : declaredFields) {
             // 判断属性是否被InjectView注解声明
             if (filed.isAnnotationPresent(InjectView.class)) {
                 InjectView injectView = filed.getAnnotation(InjectView.class);
-                //获得了注解中设置的id
+                // 获得了注解中设置的id
                 int id = injectView.value();
                 View view = activity.findViewById(id);
-                //反射设置 属性的值
-                filed.setAccessible(true); //设置访问权限，允许操作private的属性
+                // 设置访问权限，允许操作private的属性
+                filed.setAccessible(true);
                 try {
-                    //反射赋值
+                    // 反射赋值
                     filed.set(activity, view);
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
@@ -49,31 +49,32 @@ public class InjectUtils {
 
     public static void injectAutoWried(Activity activity) {
         Class<? extends Activity> cls = activity.getClass();
-        //获得数据
+        // 获得数据
         Intent intent = activity.getIntent();
         Bundle extras = intent.getExtras();
         if (extras == null) {
             return;
         }
 
-        //获得此类所有的成员
+        // 获得此类所有的成员
         Field[] declaredFields = cls.getDeclaredFields();
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(AutoWried.class)) {
-                AutoWried autowired = field.getAnnotation(AutoWried.class);
-                //获得key
-                String key = TextUtils.isEmpty(autowired.value()) ? field.getName() : autowired.value();
+                AutoWried autoWried = field.getAnnotation(AutoWried.class);
+                // 获得key
+                String key = TextUtils.isEmpty(autoWried.value()) ? field.getName() : autoWried.value();
 
                 if (extras.containsKey(key)) {
                     Object obj = extras.get(key);
-                    // todo Parcelable数组类型不能直接设置，其他的都可以.
-                    //获得数组单个元素类型
+                    // Parcelable数组类型不能直接设置，其他的都可以
+
+                    // 获得数组单个元素类型
                     Class<?> componentType = field.getType().getComponentType();
-                    //当前属性是数组并且是 Parcelable（子类）数组
+                    // 当前属性是数组并且是 Parcelable（子类）数组
                     if (field.getType().isArray() &&
                             Parcelable.class.isAssignableFrom(componentType)) {
                         Object[] objs = (Object[]) obj;
-                        //创建对应类型的数组并由objs拷贝
+                        // 创建对应类型的数组并由objs拷贝
 
                         Object[] objects = Arrays.copyOf(objs, objs.length, (Class<? extends Object[]>) field.getType());
                         obj = objects;
@@ -86,7 +87,7 @@ public class InjectUtils {
                         e.printStackTrace();
                     }
                 }
-            }
-        }
+            } // end if
+        } // end for
     }
 }
