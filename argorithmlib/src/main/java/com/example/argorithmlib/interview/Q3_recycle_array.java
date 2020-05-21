ccaebbd227bdc481df82a4faf8d761f289ff9612
justinworkshop <justinworkshop@163.com>
2020-05-21 14:52:58 +0800

    循环数组找最小

---
+++ argorithmlib/src/main/java/com/example/argorithmlib/interview/Q3_recycle_array.java
@@ -0,0 +1,39 @@
+package com.example.argorithmlib.interview;
+
+/**
+ * Copyright (C), 2016-2020
+ * FileName: Q3_recycle_array
+ * Author: zhengwei
+ * Date: 2020-05-21 14:23
+ * Description: 循环数组中找数据
+ */
+public class Q3_recycle_array {
+    public static void main(String[] args) {
+        int[] array = {50, 52, 63, 64, 65, 68, 88, 90, 98, 3, 8, 15, 44, 45, 46, 49};
+
+        System.out.println("min value: " + findMinInArray(array, 0, array.length - 1));
+    }
+
+    private static int findMinInArray(int[] array, int left, int right) {
+        System.out.println("left: " + array[left] + ", right: " + array[right]);
+
+        // 无环
+        if (array[left] < array[right]) {
+            return array[left];
+        }
+
+        if (right - left == 1) {
+            return array[left] > array[right] ? array[right] : array[left];
+        }
+
+        // 有环
+        int mid = (left + right) / 2;
+        System.out.println("mid value: " + array[mid]);
+
+        if (array[left] < array[mid]) {
+            return findMinInArray(array, mid + 1, right);
+        } else {
+            return findMinInArray(array, left, mid);
+        }
+    }
+}
